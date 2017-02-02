c3c1ee7b184854e8bba986860cd4cfcb9600513a
sk2sat <sksat.aesc@gmail.com>
2017-02-02 12:55:03 +0900

    update autorun script.

--- autoexec.sh
+++ autoexec.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
+alias tweet="~/python/twitter/tweet.py"
+
+tweet "starting autoexec.sh"
 make run
+tweet "finish simulation."
 make conv
+tweet "finish convert."
 make render
+tweet "finish rendering."
 make mp4
+tweet "finish making mp4."
